6d8f7d14d1a0e979ecbee2cee650c06eb7f459bc
dwiw96 <dwiwahyudi1996@gmail.com>
2023-11-22 23:48:55 +0700

    check if the request is canceled or deadline is exceeded by checking the context from request

---
+++ api/check.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func checkContext(ctx context.Context) error {
+	if ctx.Err() == context.Canceled {
+		log.Println("request is canceled")
+		return status.Error(codes.Canceled, "request is exceeded")
+	}
+
+	if ctx.Err() == context.DeadlineExceeded {
+		log.Println("deadline exceeded")
+		return status.Error(codes.DeadlineExceeded, "deadline is exceeded")
+	}
+
+	return nil
+}
