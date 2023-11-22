43b31f4fa79a14ef4e162a80ea0cd95bd20938f3
dwiw96 <dwiwahyudi1996@gmail.com>
2023-11-22 22:43:15 +0700

    load app.env to get data like address from that file

---
+++ tools/config.go
@@ -0,0 +1,32 @@
+package tools
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBAddress     string `mapstructure:"DB_ADDRESS"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig() takes a 'path' as input, and return 'config' object or error.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	// viper.AutomaticEnv() to tell viper to automatically override values that it has read
+	// from config file with the values of the corresponding environment variables if they exist.
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return
+	}
+	return
+}
